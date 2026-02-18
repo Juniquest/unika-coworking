5426e553135a5291daeca1860cd02e5354f7b0d5
Juniquest <RIOSTORAGECUBE@GMAIL.COM>
2026-02-18 13:46:57 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 const app = express();
 const path = require('path');
-
+app.use(cors()); //
 app.use(express.json());
 app.use(express.static(__dirname));
 
